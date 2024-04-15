877c10cb89a872748fe47489e5a26facb9108fb8
agung343 <agungwahydi343@gmail.com>
2024-04-15 22:04:58 +0700

    components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import banner from "./assets/banner.png";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="container m-auto">
+        <header>
+          <h1>Solid Crash Course</h1>
+        </header>
+
+        <img src={banner} alt="site banner" class="rounded-md" />
+        <div class="grid grid-cols-4 gap-10 my-4">
+          <Card rounded={true} flat={false}>
+            <h2 class="font-bold text-center">Studio</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ulla
+              harum velit prorro autaem odia!
+            </p>
+            <button class="btn">View</button>
+          </Card>
+          <Card rounded={false} flat={true}>
+            <h2 class="font-bold text-center">Monita 191</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ulla
+              harum velit prorro autaem odia!
+            </p>
+            <button class="btn">View</button>
+          </Card>
+        </div>
+      </div>
+    </>
   );
 }
 
